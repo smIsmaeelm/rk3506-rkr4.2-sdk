19f5cdd6a28ebed71d7096d055f267160beb2f95
jack@armsom.org <yidiantongxin>
2025-09-08 19:11:02 +0800

    fix: buildroot qt5wayland

--- buildroot/package/qt5/qt5wayland/qt5wayland.mk
+++ buildroot/package/qt5/qt5wayland/qt5wayland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5WAYLAND_VERSION = b8f1882df7215b6e00f66b7a6a08afafe230de29
+QT5WAYLAND_VERSION = 88c8df4eab81316bc055c8be82b0febc70330dbe
 QT5WAYLAND_SITE = $(QT5_SITE)/qtwayland/-/archive/$(QT5WAYLAND_VERSION)
 QT5WAYLAND_SOURCE = qtwayland-$(QT5WAYLAND_VERSION).tar.bz2
 QT5WAYLAND_DEPENDENCIES = wayland
